18ac27d45779711e8372a7085996e6fe24edd65e
MakotoInomata <inomar1486@gmail.com>
2018-06-20 00:55:43 +0900

    f q01

---
+++ js/q01.js
@@ -0,0 +1,16 @@
+String.prototype.reverse = function () {
+    return this.split('').reverse().join('')
+}
+
+let i = 11
+
+while (true) {
+    if ( (i.toString() == i.toString().reverse()) &&
+         (i.toString(2) == i.toString(2).reverse()) &&
+         (i.toString(8) == i.toString(8).reverse())
+    ) {
+        console.log(i.toString(), i.toString(2), i.toString(8))
+        break
+    }
+    i++
+}
