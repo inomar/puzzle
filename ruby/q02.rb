aebdd47b85776cb970b10535dc0fe0eafaeae86f
MakotoInomata <inomar1486@gmail.com>
2018-06-21 00:49:24 +0900

    refactor q02

--- ruby/q02.rb
+++ ruby/q02.rb
@@ -4,12 +4,9 @@ opt = ['+', '-', '*', '/', '']
    opt.each do |o|
     opt.each do |p|
       opt.each do |t|
-        next if (num[1].to_i == 0 && o == '/') || 
-        (num[2].to_i == 0 && p == '/') || 
-        (num[3].to_i == 0 && t == '/')
-        next if (num[1].to_i == 0 && p == '') ||
-        (num[2].to_i == 0 && t == '')
-        next if o == '' && p == '' && t ==''
+        next if o.empty? && p.empty? && t.empty?
+        next if (num[1].to_i.zero? && p.empty?) || (num[2].to_i.zero? && t.empty?)
+        next if (num[1].to_i.zero? && o == '/') || (num[2].to_i.zero? && p == '/') || (num[3].to_i.zero? && t == '/')
         operator = [ num[0], o, num[1], p, num[2], t, num[3] ].join
         puts "#{i} : #{operator} = #{eval(operator)}" if eval(operator).to_s.reverse == i.to_s
       end
