4b244fbd2fa55734e32f76dd029696c6047a96c7
MakotoInomata <inomar1486@gmail.com>
2018-07-09 23:55:28 +0900

    update

--- ruby/q05.rb
+++ ruby/q05.rb
@@ -23,4 +23,17 @@ def calucurate2(num)
     cnt
 end
 
-puts calucurate2(1000)
+@cnt = 0
+def change(target, coins, usable)
+  coin = coins.shift
+  if coins.size == 0
+    @cnt += 1 if (target / coin) <= usable
+  else
+    (0..target/coin).each do |i|
+      change(target - coin * i, coins.clone, usable -i)
+    end
+  end
+  @cnt
+end
+
+puts change(1000, [500, 100, 50, 10], 15)
