c8992d8814872b597ffa0c57ead62df2ebea87f3
MakotoInomata <inomar1486@gmail.com>
2018-07-19 00:02:45 +0900

    fix q06-1#

--- ruby/q06-1.rb
+++ ruby/q06-1.rb
@@ -1,7 +1,10 @@
-(1..1000).each do |i|
+num = []
+(1..10000).each do |i|
     j = i * 3 + 1
-    k = 0
     loop do
-      k = j.even? ? j / 2 : j * 3 + 1
+      j = j.even? ? j / 2 : j * 3 + 1
+      break 0 if j == 1
+      break num << i if j == i
     end
 end
+puts num.count
